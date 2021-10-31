142c9e6293d1c9becd538aa4babeed9d9fa52dc9
Daniel Bulatov <danielbul2006@gmail.com>
2021-10-31 18:50:40 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+
+#create a Flask instance
+app = Flask(__name__)
+
+#create a route decorator
+@app.route('/') 
+def index():
+	f_name = "Daniel"
+	return render_template('index.html', f_name=f_name)
+
+#localhost:5000/user/dani
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+#invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+
+#internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'), 500
